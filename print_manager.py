daf6e25e665a244b4a6a22addbe73ab555c91292
Mwaiko <komomwai2005@gmail.com>
2025-06-24 18:41:00 +0300

    Update print_manager.py

--- print_manager.py
+++ print_manager.py
@@ -19,9 +19,20 @@ class PrintQueueManager:
     def show_status(self):
         pass  # Print current queue state
 
-    # Module 2: Priority & Aging System
     def apply_priority_aging(self):
-        pass  # Implement aging logic and reorder queue
+        self.time_since_last_aging += 1
+        
+        if self.time_since_last_aging >= self.aging_interval:
+            for job in self.queue:
+                if not job['being_printed']:
+                    job['priority'] = max(1, job['priority'] - 1)  
+                    
+            self.time_since_last_aging = 0
+            
+        self.queue.sort(key=lambda x: (x['priority'], x['waiting_time']))
+    
+    def _sort_queue_by_priority(self):
+        self.queue.sort(key=lambda x: (x['priority'], x['waiting_time']))  # Implement aging logic and reorder queue
 
     # Module 3: Job Expiry & Cleanup
     def remove_expired_jobs(self):
