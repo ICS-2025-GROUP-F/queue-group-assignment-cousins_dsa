50098fb3243d3e113fb8f09fb8aee51c791e8d6b
shylynn07 <shylynn.wanjiru@strathmore.edu>
2025-06-24 22:27:58 +0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from print_queue_manager import PrintQueueManager
 
 if __name__ == "__main__":
     pq_manager = PrintQueueManager()
-    user_id, job_id, priority = 2, 2, 3
+    user_id, job_id, priority = "2", "2", 3
 
     pq_manager.enqueue_job(user_id, job_id, priority)
     pq_manager.tick()
